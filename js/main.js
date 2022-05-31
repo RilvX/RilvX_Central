df2ef66e6efb6058188e4d7b2f886b43ce3c2573
Atticus Denewmont <>
2022-05-31 19:52:42 +0000

    Added working status. Code not efficient.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
-let status = {
+let statusType = {
   blockchain: 100,
   ide: 100,
   backend: 100,
   frontend: 100,
-  api: 100,
-  site: 100
+  auth: 100,
+  host: 100,
+  rilvx: 100
 }
 
-function getStatus() {
-  
+function blockchainStatus() {
+    document.getElementById("status").innerHTML =     "Blockchain Status: " + statusType.blockchain
 }
+function ideStatus() {
+    document.getElementById("status").innerHTML =     "IDE Status: " + statusType.ide
+}
+function backendStatus() {
+    document.getElementById("status").innerHTML =     "Backend Status: " + statusType.backend
+}
+function frontendStatus() {
+    document.getElementById("status").innerHTML =     "Frontend Status: " + statusType.frontend
+}
+function authStatus() {
+    document.getElementById("status").innerHTML =     "Auth Status: " + statusType.auth
+}
+function hostStatus() {
+    document.getElementById("status").innerHTML =     "Host Status: " + statusType.host
+}
+function rilvxStatus() {
+    document.getElementById("status").innerHTML =     "RilvX Status: " + statusType.rilvx
+}
+
