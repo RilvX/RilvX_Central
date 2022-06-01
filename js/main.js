3925589bc29129489e4c8b97dfcc5840c873ba2a
RilvX <67970378+RilvX@users.noreply.github.com>
2022-06-01 00:57:26 +0000

    Added percent to status display.

--- js/main.js
+++ js/main.js
@@ -9,24 +9,24 @@ let statusType = {
 }
 
 function blockchainStatus() {
-    document.getElementById("status").innerHTML =     "Blockchain Status: " + statusType.blockchain
+    document.getElementById("status").innerHTML = "Blockchain Status: " + statusType.blockchain + "%"
 }
 function ideStatus() {
-    document.getElementById("status").innerHTML =     "IDE Status: " + statusType.ide
+    document.getElementById("status").innerHTML = "IDE Status: " + statusType.ide + "%"
 }
 function backendStatus() {
-    document.getElementById("status").innerHTML =     "Backend Status: " + statusType.backend
+    document.getElementById("status").innerHTML = "Backend Status: " + statusType.backend + "%"
 }
 function frontendStatus() {
-    document.getElementById("status").innerHTML =     "Frontend Status: " + statusType.frontend
+    document.getElementById("status").innerHTML = "Frontend Status: " + statusType.frontend + "%"
 }
 function authStatus() {
-    document.getElementById("status").innerHTML =     "Auth Status: " + statusType.auth
+    document.getElementById("status").innerHTML = "Auth Status: " + statusType.auth + "%"
 }
 function hostStatus() {
-    document.getElementById("status").innerHTML =     "Host Status: " + statusType.host
+    document.getElementById("status").innerHTML = "Host Status: " + statusType.host + "%"
 }
 function rilvxStatus() {
-    document.getElementById("status").innerHTML =     "RilvX Status: " + statusType.rilvx
+    document.getElementById("status").innerHTML = "RilvX Status: " + statusType.rilvx + "%"
 }
 
