e91c5a248e6780e2a71b3cc2cd5f1b7cf171661b
RilvX <67970378+RilvX@users.noreply.github.com>
2022-06-01 02:14:28 +0000

    Status color

--- js/main.js
+++ js/main.js
@@ -1,32 +1,48 @@
 let statusType = {
   blockchain: 100,
-  ide: 100,
+  ide: "Critical",
   backend: 100,
   frontend: 100,
   auth: 100,
   host: 100,
-  rilvx: 100
+  rilvx: "Operational"
 }
 
+let critical = false
+
 function blockchainStatus() {
-    document.getElementById("status").innerHTML = "Blockchain Status: " + statusType.blockchain + "%"
+  document.getElementById("status").innerHTML = "Blockchain Status: " + statusType.blockchain + "%"
+
+  document.getElementById('status').style.backgroundColor = 'black';
 }
 function ideStatus() {
-    document.getElementById("status").innerHTML = "IDE Status: " + statusType.ide + "%"
+  document.getElementById("status").innerHTML = "IDE Status: " + statusType.ide
+  
+  document.getElementById('status').style.backgroundColor = 'red';
 }
 function backendStatus() {
-    document.getElementById("status").innerHTML = "Backend Status: " + statusType.backend + "%"
+  document.getElementById("status").innerHTML = "Backend Status: " + statusType.backend + "%"
+
+  document.getElementById('status').style.backgroundColor = 'black';
 }
 function frontendStatus() {
-    document.getElementById("status").innerHTML = "Frontend Status: " + statusType.frontend + "%"
+  document.getElementById("status").innerHTML = "Frontend Status: " + statusType.frontend + "%"
+
+  document.getElementById('status').style.backgroundColor = 'black';
 }
 function authStatus() {
-    document.getElementById("status").innerHTML = "Auth Status: " + statusType.auth + "%"
+  document.getElementById("status").innerHTML = "Auth Status: " + statusType.auth + "%"
+
+  document.getElementById('status').style.backgroundColor = 'black';
 }
 function hostStatus() {
-    document.getElementById("status").innerHTML = "Host Status: " + statusType.host + "%"
+  document.getElementById("status").innerHTML = "Host Status: " + statusType.host + "%"
+
+  document.getElementById('status').style.backgroundColor = 'black';
 }
 function rilvxStatus() {
-    document.getElementById("status").innerHTML = "RilvX Status: " + statusType.rilvx + "%"
+  document.getElementById("status").innerHTML = "Status: " + statusType.rilvx
+
+  document.getElementById('status').style.backgroundColor = 'black';
 }
 
